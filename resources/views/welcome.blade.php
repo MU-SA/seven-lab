730725a2a9464295b8ef5a8abbbac17d9a8bdd35
Muhammad Saleh <m7md.sal762@gmail.com>
2019-03-08 04:28:17 +0200

    initial

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Title -->
-    <title>Hami - Web Hosting HTML Template</title>
+    <title>Seven Lab</title>
 
     <!-- Favicon -->
     <link rel="icon" href="/imgs/sevenpdf.png">
